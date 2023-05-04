9edda8b20240b57c8bce48008b7abc711fbced70
kobrovac <120015883+kobrovac@users.noreply.github.com>
2023-05-04 10:16:17 +0200

    Added GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Karin Obrovac and is <a href="https://github.com/kobrovac/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
