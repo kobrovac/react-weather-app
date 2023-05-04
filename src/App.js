461a5e3e0b83f5af6366df313e64cc1916f0ca4c
kobrovac <120015883+kobrovac@users.noreply.github.com>
2023-05-04 11:10:27 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project is coded by Karin Obrovac and is <a href="https://github.com/kobrovac/react-weather-app" target="_blank" rel="noreferrer">
           open-sourced on GitHub
         </a>
       </footer>
+      </div>
     </div>
   );
 }
